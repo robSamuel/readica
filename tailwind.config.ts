30fc7d5bb561b3b133dcc5693981edb91733b411
robSamuel <robert_samuel97@hotmail.com>
2025-07-23 15:33:18 -0600

    feat: install biome, tailwind and remove eslint

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+//import { shadcnPreset } from '@shadcn/ui'; // Only needed if using ShadCN
+
+const config: Config = {
+  // presets: [shadcnPreset], // Remove this line if you're not using ShadCN yet
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;

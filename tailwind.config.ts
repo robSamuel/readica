222a3a4a73dc5733e8072e8010844ccecc2e70fb
robSamuel <robert_samuel97@hotmail.com>
2025-07-23 15:59:16 -0600

    chore: run the formatter script

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
 import type { Config } from 'tailwindcss';
+
 //import { shadcnPreset } from '@shadcn/ui'; // Only needed if using ShadCN
 
 const config: Config = {
-  // presets: [shadcnPreset], // Remove this line if you're not using ShadCN yet
-  content: [
-    './index.html',
-    './src/**/*.{ts,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	// presets: [shadcnPreset], // Remove this line if you're not using ShadCN yet
+	content: ['./index.html', './src/**/*.{ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
 
 export default config;
